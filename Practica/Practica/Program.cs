91f2051a4249ebe1ab6eee65fbfe65f4329106ec
Ronald Bismar <145393966+ronald-bismar@users.noreply.github.com>
2024-11-26 09:58:23 -0400

    Add files via upload

---
+++ Practica/Practica/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Windows
+ * Date: 25/9/2023
+ * Time: 22:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Practica
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
